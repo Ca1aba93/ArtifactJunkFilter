ae7e2ab9c185d9ccb3653445c89063450993a998
Tory4Github <109908461+Tory4Github@users.noreply.github.com>
2022-10-13 23:28:06 +0800

    生成yas-lock 1.07 lock.json
    用yas-lock.exe 扫描后,筛选完再次运行yas-lock.exe 可以直接加锁潜在圣遗物ps:测试使用yaslock 1.07.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from configparser import ConfigParser
 
 import pandas as pd
 
-from artifact import Artifact, artifacts
+from artifact import Artifact, artifacts,good_arts
 from build import build_df
 
 proDir = os.path.split(os.path.realpath(__file__))[0]
@@ -78,10 +78,12 @@ if __name__ == '__main__':
 
     art_d = {}
     p_ = 0
+    idx = 0
+    lock=[0]*2000
     for artifact in artifacts:
         a = Artifact()
         a.read(artifact)
-        temp_best = 0
+        temp_best = 0   
         if a.rarity > 0:
             temp_best, temp_art_df = adapt(a, build_df)
             if temp_best > score_threshold or a.rarity > rarity_threshold:  # 筛选最佳适配度或稀有度达标的圣遗物
@@ -92,13 +94,18 @@ if __name__ == '__main__':
                 art_score['artRarity'] = a.rarity
                 art_score['bestScore'] = temp_best
                 art_score['eachScore'] = temp_art_df
+                art_score['index'] = idx
+                lock[idx]=1
                 art_d[p_] = art_score
                 p_ += 1
+        idx  =idx + 1
     all_score = pd.DataFrame(art_d).T
     all_score = all_score.sort_values(by='bestScore', ascending=False)
     print('共有{}件圣遗物, 显示其中{}件'.format(len(artifacts), all_score.shape[0]))
     for i in range(all_score.shape[0]):
         print('<============================【{}】============================>'.format(i+1))
+        ##print('<============================【{}】============================>'.format(i+1))
+        print(all_score.iloc[i]['index'])
         print(all_score.iloc[i]['artAbstract'])
         print(all_score.iloc[i]['artMain'])
         print(all_score.iloc[i]['artSec'])
@@ -113,6 +120,73 @@ if __name__ == '__main__':
             print('对BUILD:[{}]的适配度为【{}】,该部位完美毕业难度为: {}'.format(
                 temp.iloc[j]['buildName'], '{:.1%}'.format(temp.iloc[j]['adaptScore']),
                 '{:.1f}'.format(temp.iloc[j]['difficulty'])))
+
+    idx=0
+    plume=0##羽
+    sands=0##沙
+    flower=0##花
+    circlet=0##头
+    goblet=0##杯
+    idx_convert=0
+    for good_art in good_arts:
+        if(good_art['slotKey']=='flower'): 
+            flower=flower+1
+        if(good_art['slotKey']=='plume'): 
+            plume=plume+1
+        if(good_art['slotKey']=='sands'): 
+            sands=sands+1
+        if(good_art['slotKey']=='goblet'): 
+           goblet=goblet+1
+        if(good_art['slotKey']=='circlet'): 
+            circlet=circlet+1
+    bias_flower=0      
+    bias_plume =flower 
+    bias_sands =flower+plume 
+    bias_goblet=flower+plume+sands 
+    bias_circlet=flower+plume+sands+goblet 
+
+
+    plume=0##羽
+    sands=0##沙
+    flower=0##花
+    circlet=0##头
+    goblet=0##杯
+    idx_convert=0    
+    list_lock=[]
+    for good_art in good_arts:
+        ##a.read(good_art)
+        ##a.read(good_art)
+        
+       ##print( good_art['slotKey'])
+        if(good_art['slotKey']=='flower'): 
+            idx_convert=bias_flower+flower
+            flower=flower+1
+        if(good_art['slotKey']=='plume'): 
+            idx_convert=bias_plume +plume
+            plume=plume+1
+        if(good_art['slotKey']=='sands'):
+            idx_convert=bias_sands +sands 
+            sands=sands+1
+        if(good_art['slotKey']=='goblet'): 
+            idx_convert=bias_goblet+goblet 
+            goblet=goblet+1
+        if(good_art['slotKey']=='circlet'): 
+            idx_convert=bias_circlet+circlet 
+            circlet=circlet+1
+            ##print('circlet+1')
+        ##idx_convert = flower+plume+sands+goblet+circlet
+        if (lock[idx_convert]==1):
+            ##print( good_art)
+            if(good_art['lock']==False):
+                list_lock.append(idx)
+            ##print('<============mona==============【{}】============================>'.format(idx_convert))
+            ##print('<============good================【{}】============================>'.format(idx))
+        idx += 1
+    print_lock = open("lock.json",'w')
+    print(list_lock,file = print_lock)
+    print_lock.close()
+    print('需要加锁good.json以下圣遗物')   
+    print(list_lock)              
     input('请按任意键继续...')
     time.sleep(999)
     print('999秒后自动关闭窗口')
