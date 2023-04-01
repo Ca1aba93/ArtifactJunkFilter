8e1a67dad896185a7d77dd6d46efc6d47bdd3e8e
Ca1aba93 <554641082@qq.com>
2023-04-01 18:56:10 +0800

    test

--- src/artifact_data_analysis.py
+++ src/artifact_data_analysis.py
@@ -25,6 +25,10 @@ pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 
 
+def test():
+    pass
+
+
 def check_main_stat(artifact_id: int, config_id: int):
     artifact = Artifact(artifact_id)
     config = XZSConfig(config_id)
